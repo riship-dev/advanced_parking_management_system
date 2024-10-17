21c008ddd56be81312a5a381ffc9e6d549d27bdd
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-17 08:11:40 +0530

    Initialise with forward declarations

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <vector>
+using namespace std;
+
+class Vehicle;
+class Car; // class Car : public Vehicle;
+class Bike; // class Bike : public Vehicle;
+class Bus; //class Bus : public Vehicle;
+class ParkingSlot;
+class ParkingLot;
+class Payment;
+class Admin;
+
+int main() {
+
+}
