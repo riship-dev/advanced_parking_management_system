48146945717e88d21f44d8c06e956a3b0e5dcad0
Rishi <45505953+whileTruegrind@users.noreply.github.com>
2024-10-17 08:36:34 +0530

    Design "Vehicle" base class

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Vehicle;
 class Car; // class Car : public Vehicle;
 class Bike; // class Bike : public Vehicle;
-class Bus; //class Bus : public Vehicle;
+class Bus; // class Bus : public Vehicle;
 class ParkingSlot;
 class ParkingLot;
 class Payment;
@@ -16,3 +16,25 @@ class Admin;
 int main() {
 
 }
+
+class Vehicle {
+    private:
+        string licensePlate;
+        string type;
+        time_t entryTime;
+        time_t exitTime;
+    protected:
+        void setEntryTime() { time(&entryTime); }
+        void setExitTime() { time(&exitTime); }
+    public:
+        Vehicle(string licensePlate, string type) : licensePlate(licensePlate), type(type) { setEntryTime(); }
+        
+        string getLicensePlate() const { return licensePlate; }
+        string getType() const { return type; }
+        
+        virtual void park() { cout << "Vehicle with license plate " << licensePlate << " is parked." << endl; }
+        virtual void exit() {
+            setExitTime();
+            cout << "Vehicle with license plate " << licensePlate << " is exiting." << endl;
+        }
+};
